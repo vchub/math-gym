9c990ca6ed8b1b64df1a1022c3d7520788f93620
vlad chub <vlad.chub@gmail.com>
2025-09-01 13:04:59 -0700

    rm src/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,41 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration using environment variables
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCZp1u9z6z9FdIIr6jfS2y4MLL31zTMu3I",
+  authDomain: "math-gym-e1325.firebaseapp.com",
+  projectId: "math-gym-e1325",
+  storageBucket: "math-gym-e1325.firebasestorage.app",
+  messagingSenderId: "814757267933",
+  appId: "1:814757267933:web:ece2217ac16a1124d14e34",
+  measurementId: "G-F2LXQLLR7E"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+// const analytics = getAnalytics(app);
 // Initialize and export Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+
+// // Your web app's Firebase configuration using environment variables
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+// };
+
+
